c3ef2cef362bb01fa12a5a2bf003d9d3f3866ebe
clement32112 <totoobieke@gmail.com>
2023-09-10 21:01:06 +0100

    Added script template

---
+++ script_templates/state/state.gd
@@ -0,0 +1,19 @@
+extends state
+
+func _ready() -> void:
+	super._ready()
+
+## function runs when the current state has been transitioned into
+func _enter_state() ->void:
+	pass
+## funtion that runs in place of _process controled by the parent state machine
+func _process_contorl(delta)->void:
+
+	pass
+## function that runs in place of _physics_process controlled by the parent state machine
+func _physic_control(delta)->void:
+	pass
+## function that handles all exit task done before leaving the state
+func _exit_state()->void:
+	
+	pass
